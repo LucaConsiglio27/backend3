ff6302adaef5612a4ad169e0694d8bf51c3b77d0
LucaConsiglio27 <lsconsiglio@hotmail.com>
2024-11-06 11:46:21 -0300

    Primer Entrega

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,17 +1,24 @@
-// src/config/logger.js
-const { createLogger, format, transports } = require('winston');
+const winston = require('winston');
 
-const levels = { debug: 0, http: 1, info: 2, warning: 3, error: 4, fatal: 5 };
-
-const logger = (environment) => {
-  const fileTransport = new transports.File({ filename: 'errors.log', level: 'error' });
-  const consoleTransport = new transports.Console({ format: format.combine(format.colorize(), format.simple()) });
-
-  return createLogger({
-    levels,
-    level: environment === 'production' ? 'info' : 'debug',
-    transports: environment === 'production' ? [consoleTransport, fileTransport] : [consoleTransport],
-  });
-};
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [
+    // Transporte de consola
+    new winston.transports.Console({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    }),
+    // Transporte de archivo para errores
+    new winston.transports.File({
+      filename: 'error.log',
+      level: 'error', // Solo guarda errores o niveles más altos
+    }),
+  ],
+});
 
 module.exports = logger;
